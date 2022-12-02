2b86564742cd4a641d310b324c94dfa5955cbcf2
Ivy Sifuma <ivy.sifuma@student.moringaschool.com>
2022-12-02 13:38:33 +0300

    Done

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,19 +1,22 @@
 class PlantsController < ApplicationController
     def index
-        render json: Plant.all, status: :ok
+        render json: Plant.all, except: ["created_at", "updated_at"], status: :ok
       end
+    
       def show
         plant = Plant.find_by(id: params[:id])
-        if plant
-        render json: plant
-       else
-        render json: {error: "Plant not found"}, status: :not_found
-       end
-       end
+        if plant 
+          render json: plant, except: ["created_at", "updated_at"]
+        else
+          render json: {error: "Couldn't find plant"}, status: :not_found
+        end
+      end
+    
       def create
         plant = Plant.create(plant_params)
-        render json: plant, status: :created
+        render json: plant, except: ["created_at", "updated_at"], status: :created
       end
+    
       private
       def plant_params
         params.permit(:name, :image, :price)

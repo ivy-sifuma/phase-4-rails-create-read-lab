2805b79ff7d54a99ae80b733e0cafc0824259ceb
Ivy Sifuma <ivy.sifuma@student.moringaschool.com>
2022-12-02 13:52:08 +0300

    Done

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_02_101501) do
+ActiveRecord::Schema.define(version: 2022_12_02_104902) do
 
   create_table "plants", force: :cascade do |t|
     t.string "name"
     t.string "image"
-    t.integer "price"
+    t.decimal "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
